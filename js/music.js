6e5e4b5338bb614bdac3c543fd9d84bf2fad8eb0
fazemodz <hike820@gmail.com>
2021-06-22 05:34:09 +0100

    initial submit

---
+++ js/music.js
@@ -0,0 +1,135 @@
+var randNum = Math.floor(Math.random() * 9);
+if (randNum == 1) {
+   
+        var audio = new Audio('https://kpoparchives.com/music/thankstom.mp3');
+        function playSong() {
+            audio.play()
+             .catch(() => {
+                setTimeout(function() {
+                    playSong()
+                }, 100);
+            });
+        }
+        playSong()
+        audio.volume = 0.25;
+       
+    }
+    if (randNum == 2) {
+        var audio = new Audio('https://music.kpoparchives.com/[MV]_(G)I-DLE((%EC%97%AC%EC%9E%90)%EC%95%84%EC%9D%B4%EB%93%A4)___Senorita.mp3');
+        function playSong() {
+            audio.play()
+             .catch(() => {
+                setTimeout(function() {
+                    playSong()
+                }, 100);
+            });
+        }
+        playSong()
+        audio.volume = 0.15;
+    }
+    if (randNum == 3) {
+   
+        var audio = new Audio('https://music.kpoparchives.com/(Daichi_Miura)Blizzard.mp3');
+        function playSong() {
+            audio.play()
+             .catch(() => {
+                setTimeout(function() {
+                    playSong()
+                }, 100);
+            });
+        }
+        playSong()
+        audio.volume = 0.15;
+    }
+
+    if (randNum == 4) {
+        var audio = new Audio('https://kpoparchives.com/music/K-DA%20-%20POP-STARS%20(ft%20Madison%20Beer,%20(G)I-DLE,%20Jaira%20Burns)%20_%20Official%20Music%20Video%20-%20League%20of%20Legends.mp3');
+        function playSong() {
+            audio.play()
+             .catch(() => {
+                setTimeout(function() {
+                    playSong()
+                }, 100);
+            });
+        }
+        playSong()
+        audio.volume = 0.15;
+    }
+    if (randNum == 5) {
+        var audio = new Audio('https://kpoparchives.com/music/bitch%20lasagna.mp3');
+        function playSong() {
+            audio.play()
+             .catch(() => {
+                setTimeout(function() {
+                    playSong()
+                }, 100);
+            });
+        }
+        playSong()
+        audio.volume = 0.15;
+    }
+    if (randNum == 6) {
+        var audio = new Audio('https://kpoparchives.com/music/ATEEZPirate%20King.mp3');
+        function playSong() {
+            audio.play()
+             .catch(() => {
+                setTimeout(function() {
+                    playSong()
+                }, 100);
+            });
+        }
+        playSong()
+        audio.volume = 0.15;
+    }
+    if (randNum == 7) {
+        var audio = new Audio('https://kpoparchives.com/music/%5bMV%5d(LOONA)%20Butterfly.mp3');
+        function playSong() {
+            audio.play()
+             .catch(() => {
+                setTimeout(function() {
+                    playSong()
+                }, 100);
+            });
+        }
+        playSong()
+        audio.volume = 0.15;
+    }
+    if (randNum == 8) {
+        var audio = new Audio('https://music.kpoparchives.com/JENNIE%20-%20SOLO%20M-V.mp3');
+        function playSong() {
+            audio.play()
+             .catch(() => {
+                setTimeout(function() {
+                    playSong()
+                }, 100);
+            });
+        }
+        playSong()
+        audio.volume = 0.15;
+    }
+    if (randNum == 9) {
+        var audio = new Audio('https://music.kpoparchives.com/%EC%97%AC%EC%9E%90%EC%95%84%EC%9D%B4%EB%93%A4GI-DLE%20HANN%20Alone%20Official%20Music%20Video.mp3');
+        function playSong() {
+            audio.play()
+             .catch(() => {
+                setTimeout(function() {
+                    playSong()
+                }, 100);
+            });
+        }
+        playSong()
+        audio.volume = 0.15;
+    }
+    if (randNum == 9) {
+        var audio = new Audio('https://music.kpoparchives.com/EVERGLOW%20-(Bon%20Bon%20Chocolat)%20MV.mp3');
+        function playSong() {
+            audio.play()
+             .catch(() => {
+                setTimeout(function() {
+                    playSong()
+                }, 100);
+            });
+        }
+        playSong()
+        audio.volume = 0.15;
+    }
